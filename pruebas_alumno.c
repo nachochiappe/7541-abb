e601ba32bdc145a399ef448389a3de4e2b6b76b1
Ignacio Chiappe <nacho.chiappe@gmail.com>
2015-10-16 13:00:50 -0300

    Versiones iniciales. No tienen nada.

---
+++ pruebas_alumno.c
@@ -0,0 +1,24 @@
+#include "abb.h"
+#include "testing.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* ******************************************************************
+ *                        PRUEBAS UNITARIAS
+ * *****************************************************************/
+
+
+
+/* ******************************************************************
+ *                        FUNCIÓN PRINCIPAL
+ * *****************************************************************/
+
+
+void pruebas_abb_alumno()
+{
+    /* Ejecuta todas las pruebas unitarias. */
+    
+}
